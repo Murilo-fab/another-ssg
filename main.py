27001ebd3dc7322782cd9bf791beb1cf4972c14d
Murilo-fab <murilox26@hotmail.com.br>
2026-05-30 00:02:40 -0300

    Change H1 index

--- main.py
+++ main.py
@@ -118,6 +118,8 @@ def build_pages(content_dir, output_dir, boilerplate, config):
     return notes_index
 
 def build_index(notes_index, boilerplate, output_dir, config):
+    html_notes = "<h1>📝 Index</h1>"
+
     if not notes_index:
         html_notes += "<p>Nothing to see here.</p>"
     else:
